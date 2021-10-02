9e6f852885841b55484f99af12217965f332b2a2
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-02 13:00:18 +0200

    section 3 - 13(Build a Temperature Converter)
    Build a Temperature Converter

---
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/basics/Hello-world/temp-conversion.js
@@ -0,0 +1,28 @@
+let fahrenheit = 32
+celsius = (fahrenheit - 32) / 1.8
+console.log('Fahrenheit to Celsius: ' + celsius)
+
+kelvin = (fahrenheit + 459.67) * 5/9
+console.log('Fahrenheit to Kelvin: ' + kelvin)
+
+
+
+// let fahrenheit = 32,
+// choose = 0,
+// loop =1
+// while(loop != 0){
+//     console.log('To convert from fahrenheit to celsius press 1\nTo convert from fahrenheit to kelvin press 2')
+//     if (choose == 1) {
+//         celsius = (fahrenheit - 32) / 1.8
+//         console.log(celsius)
+//     }else if (choose == 2) {
+//         kelvin = (fahrenheit + 459.67) * 5/9
+//         console.log(kelvin)
+//     }else {
+//         console.log('please select from 1 or 2 only')
+//     }
+//     console.log('if you want convert again press any number to end press 0')
+// }
+
+
+
