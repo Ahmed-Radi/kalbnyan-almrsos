17471108f2392b8433292b1883b2127cdf4e4668
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-02 13:02:01 +0200

    section 3 - 15(Booleans and Comparison Operators)

---
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/basics/Hello-world/boolean.js
@@ -0,0 +1,11 @@
+let age = 6 //chiled chiled discounts
+// let age = 65 //senior chiled discounts
+// let age = 65 //senior chiled discounts
+// let age = 32 //no output
+
+if(age <= 7) {
+    console.log('chiled discounts')
+}else if(age >= 65) {
+    console.log('senior discounts')
+}
+
