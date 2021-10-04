01f73c3660cc609a5315649cd930f373ea48eccb
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-04 21:12:14 +0200

    section 4 - 26(Build a Grade Calculator)
    Build a Grade Calculator

---
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/functions/grade-calc.js
@@ -0,0 +1,39 @@
+function grade(grade, total=100) { //the default for the total grade is 100%
+    let gradePresentage = (grade / total)*100;
+
+    // if(gradePresentage <= 100 && gradePresentage >= 95)
+    if(gradePresentage >= 95){
+        letter = "A+"
+    }else if(gradePresentage <= 94.99 && gradePresentage >= 90){
+        letter = "A"
+    }else if(gradePresentage <= 89.99 && gradePresentage >= 85){
+        letter = "B+"
+    }else if(gradePresentage <= 84.99 && gradePresentage >= 80){
+        letter = "B"
+    }else if(gradePresentage <= 79.99 && gradePresentage >= 75){
+        letter = "C+"
+    }else if(gradePresentage <= 74.99 && gradePresentage >= 70){
+        letter = "C"
+    }else if(gradePresentage <= 69.99 && gradePresentage >= 65){
+        letter = "D+"
+    }else if(gradePresentage <= 64.99 && gradePresentage >= 50){
+        letter = "D"
+    }else {
+        letter = "F"
+    }
+    console.log(`You got a ${letter} ${gradePresentage}%`)
+}
+
+grade(20,20)
+grade(19,40)
+grade(80)
+grade(77)
+grade(55)
+grade(100)
+grade(14,20)
+grade(99)
+grade(12,30)
+grade(11,15)
+grade(10,20)
+grade(9,20)
+grade(77,100)
