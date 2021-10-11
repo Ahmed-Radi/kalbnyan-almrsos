555c4b7c61aa3e3e89386755c8341fa8c9896d1e
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-11 13:42:06 +0200

    section 6 - 38(Array Basics)

---
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/arrays/todo.js
@@ -0,0 +1,7 @@
+let todo = ["walkup", 'pray', 'play games', 'study', 'study again']
+console.log(`You have ${todo.length} todos`)
+console.log(`Todo ${todo.length-4}: ${todo[0]}`)
+console.log(`Todo ${todo.length-3}: ${todo[1]}`)
+console.log(`Todo ${todo.length-2}: ${todo[2]}`)
+console.log(`Todo ${todo.length-1}: ${todo[3]}`)
+console.log(`Todo ${todo.length}: ${todo[4]}`)
