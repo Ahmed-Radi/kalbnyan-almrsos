600e5758dff7927f6a280d4cdf1219d3851fbb36
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-11 15:35:30 +0200

    section 6 - 39(Manipulating Arrays with Methods)

--- 13- The Modern JavaScript Bootcamp/js-bootcamp/arrays/todo.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/arrays/todo.js
@@ -1,7 +1,11 @@
 let todo = ["walkup", 'pray', 'play games', 'study', 'study again']
+
+//delete third item
+todo.splice(2,1)
+//Add new item in the end
+todo.push("new todo")
+// remove first item
+todo.shift();
+
 console.log(`You have ${todo.length} todos`)
-console.log(`Todo ${todo.length-4}: ${todo[0]}`)
-console.log(`Todo ${todo.length-3}: ${todo[1]}`)
-console.log(`Todo ${todo.length-2}: ${todo[2]}`)
-console.log(`Todo ${todo.length-1}: ${todo[3]}`)
-console.log(`Todo ${todo.length}: ${todo[4]}`)
+console.log(todo)
