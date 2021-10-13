624ff211b4e7590a30975b43f584e64aca65c3cc
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-13 15:52:43 +0200

    section 6 - 43(Searching Arrays: Part II)

--- 13- The Modern JavaScript Bootcamp/js-bootcamp/arrays/todo.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/arrays/todo.js
@@ -1,13 +1,59 @@
-let todo = ["walkup", 'pray', 'play games', 'study', 'study again']
+let todos = [
+    {
+        text:"walkup",
+        complate: false
+    },
+    {
+        text:'pray',
+        complate:true
+    },
+    {
+        text:'play games',
+        complete: true
+    },
+    {
+        text:'study',
+        complete: false
+    },
+    {
+        text:'study again',
+        complete: true
+    }
+]
 
-//delete third item
-todo.splice(2,1)
-//Add new item in the end
-todo.push("new todo")
-// remove first item
-todo.shift();
+// const removeNote = function(todos, todoText) {
+//     const findToDo = todos.find(function(todo){
+//         return todo.text.toLowerCase() === todoText.toLowerCase()
+//     })
+//     todos.splice(todos.indexOf(findToDo), 1)
+// }
 
-console.log(`You have ${todo.length} todos`)
-todo.forEach(function(items, index) {
-    console.log(`${index+1}. ${items}`)
-})
+
+const removeNote = function(todo, todoText) {
+    const index = todo.findIndex(function(todos){
+        return todos.text.toLowerCase() === todoText.toLowerCase()
+    })
+    if(index !== -1){
+        todo.splice(index, 1)
+    }
+    return todo
+}
+console.log(removeNote(todos, 'study'))
+console.log(todos)
+
+
+
+
+
+// let todo = ["walkup", 'pray', 'play games', 'study', 'study again']
+// //delete third item
+// todo.splice(2,1)
+// //Add new item in the end
+// todo.push("new todo")
+// // remove first item
+// todo.shift();
+
+// console.log(`You have ${todo.length} todos`)
+// todo.forEach(function(items, index) {
+//     console.log(`${index+1}. ${items}`)
+// })
