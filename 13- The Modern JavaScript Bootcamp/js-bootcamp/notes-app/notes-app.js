02f5259818207966e7ab696635a83acf91871f17
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-16 01:04:28 +0200

    section 7 - 52(Adding Elements via the DOM)

--- 13- The Modern JavaScript Bootcamp/js-bootcamp/notes-app/notes-app.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/notes-app/notes-app.js
@@ -1,4 +1,18 @@
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 const ps = document.querySelectorAll('.info')
 ps.forEach(function (p) {
     p.textContent = '*******'
 })
+
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'The Ahmed Radi';
+document.querySelector('body').appendChild(newParagraph)
--- 13- The Modern JavaScript Bootcamp/js-bootcamp/todo-app/todo-app.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/todo-app/todo-app.js
@@ -1,6 +1,48 @@
-const par = document.querySelectorAll('p');
-par.forEach(function(p) {
-    if(p.textContent.includes('The') || p.textContent.includes('the')) {
-        p.remove();
+let todos = [
+    {
+        text:"walkup",
+        complete: false
+    },
+    {
+        text:'pray',
+        complete:true
+    },
+    {
+        text:'play games',
+        complete: true
+    },
+    {
+        text:'study',
+        complete: false
+    },
+    {
+        text:'study again',
+        complete: true
     }
+]
+// know un complete todos
+const countUnComplete = todos.filter(function(todos) {
+    return !todos.complete
 })
+const summary = document.createElement('h2')
+summary.textContent = `you have ${countUnComplete.length} todos left`
+document.querySelector('body').appendChild(summary)
+//Display Content
+todos.forEach(function (todos) {
+    const p = document.createElement('p')
+    p.textContent = todos.text;
+    document.querySelector('body').appendChild(p)
+})
+
+// const addText = document.createElement('p');
+// document.querySelector('body').forEach(function(text) {
+//     addText.textContent = todos.text;
+// })
+
+//Remove all Paragraph have word 'The'
+// const par = document.querySelectorAll('p');
+// par.forEach(function(p) {
+//     if(p.textContent.includes('The') || p.textContent.includes('the')) {
+//         p.remove();
+//     }
+// })
