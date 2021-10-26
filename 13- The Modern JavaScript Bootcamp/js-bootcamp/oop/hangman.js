366d57ae579e0b5c881efdaf12602eab164f125c
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-26 09:37:34 +0200

    section 10 - 92. Hangman Challenge: Part I

--- 13- The Modern JavaScript Bootcamp/js-bootcamp/oop/hangman.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/oop/hangman.js
@@ -1,10 +1,25 @@
-const Hangman = function (word, remainingGuesses) {
-    this.word = word;
+const Hangman = function (word= [], remainingGuesses) {
+    this.word = word.toLowerCase().split('');
     this.remainingGuesses = remainingGuesses;
+    this.guessedLetters = ['a']
 }
 
-const game1 = new Hangman('ahmed', 5)
-console.log(game1)
+Hangman.prototype.getPuzzle = function () {
+    let puzzel = ''
+
+        this.word.forEach((letter) => {
+            if(this.guessedLetters.includes(letter) || letter === ' ') {
+                puzzel += letter;
+            } else {
+                puzzel += '*'
+            }
+        })
+
+    return puzzel;
+}
+
+const game1 = new Hangman('Ahmed', 5)
+console.log(game1.getPuzzle())
 
 const game2 = new Hangman('ali', 3)
-console.log(game2)
+console.log(game2.getPuzzle())
