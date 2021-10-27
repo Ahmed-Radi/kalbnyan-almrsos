762992d3b7eb36404fad02ba41d933eaf3627f97
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-27 22:55:04 +0200

    section 3 - 13. ES6 Aside: const and let

--- 14- The Complete React Developer Course (w Hooks and Redux)/indecision-app/src/app.js
+++ 14- The Complete React Developer Course (w Hooks and Redux)/indecision-app/src/app.js
@@ -1,10 +1,10 @@
-var app = {
+const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
     options:['One', 'Two']
 };
 
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
@@ -16,12 +16,12 @@ var template = (
     </div>
 );
 
-var user = {
+const user = {
     name: 'Ahmed Radi',
     age: 26,
     location: 'Egypt'
 };
-var templateTwo = (
+const templateTwo = (
     <div>
         <h1>{user.name}</h1>
         <p>Age: {user.age}</p>
@@ -29,6 +29,6 @@ var templateTwo = (
     </div>
 );
 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
