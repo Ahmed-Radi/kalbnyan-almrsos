b2fcd881c88ae64c7487bfd9543496715ebef84e
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-30 11:58:17 +0200

    section 11 - 104. HTTP Requests from JavaScript

--- 13- The Modern JavaScript Bootcamp/js-bootcamp/hangman/js/app.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/hangman/js/app.js
@@ -20,3 +20,14 @@ window.addEventListener('keypress',(e) => {
 
 // check player status
 
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', (e) => {
+    if(e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText);
+        console.log(data)
+    }
+})
+
+request.open('GET','http://puzzle.mead.io/puzzle')
+request.send()
