0c079e721c1453d4ea33622f0dc72332369b7bea
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-31 15:57:46 +0200

    section 10 - 107. Callback Abstraction

--- 13- The Modern JavaScript Bootcamp/js-bootcamp/hangman/js/app.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/hangman/js/app.js
@@ -18,21 +18,14 @@ window.addEventListener('keypress',(e) => {
 
 })
 
-// check player status
-
-const request = new XMLHttpRequest();
-
-request.addEventListener('readystatechange', (e) => {
-    if(e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText);
-        console.log(data)
-    } else if(e.target.readyState === 4) {
-        console.log('An error has taken place')
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle)
     }
 })
-
-request.open('GET','http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
+// check player status
 
 const countryCode = 'EG'
 const newRequest = new XMLHttpRequest()
