b35dd101df350825343866ff4c2a21c67fcef8d5
Ahmed Radi <ahmedradi743@gmail.com>
2021-10-31 14:51:23 +0200

    section 10 - 106. Exploring Another API

--- 13- The Modern JavaScript Bootcamp/js-bootcamp/hangman/js/app.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/hangman/js/app.js
@@ -33,3 +33,18 @@ request.addEventListener('readystatechange', (e) => {
 
 request.open('GET','http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
+
+const countryCode = 'EG'
+const newRequest = new XMLHttpRequest()
+newRequest.addEventListener('readystatechange', (e) => {
+    if( e.target.readyState === 4 && e.target.status === 200 ) {
+        const data = JSON.parse(e.target.responseText)
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        console.log(country.name)
+    } else if (e.target.status === 4) {
+        console.log('Unable to fetch data')
+    }
+})
+
+newRequest.open('GET','https://restcountries.com/v2/all')
+newRequest.send()
