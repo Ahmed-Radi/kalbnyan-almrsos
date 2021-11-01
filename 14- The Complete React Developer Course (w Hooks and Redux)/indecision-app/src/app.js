dc1ca2d71d2ba7adcc92b433e9d2c5030765f1b5
Ahmed Radi <ahmedradi743@gmail.com>
2021-11-02 00:50:32 +0200

    section 4 - 30. Method Binding

--- 14- The Complete React Developer Course (w Hooks and Redux)/indecision-app/src/app.js
+++ 14- The Complete React Developer Course (w Hooks and Redux)/indecision-app/src/app.js
@@ -40,7 +40,12 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
     handleRemoveAll() {
+        console.log(this.props.options)
         alert('handleRemoveAll')
     }
     render() {
