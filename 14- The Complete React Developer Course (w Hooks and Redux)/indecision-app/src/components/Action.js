d6931872ff9aa1958d97049b780ac000256b1ee4
Ahmed Radi <ahmedradi743@gmail.com>
2021-11-13 20:10:14 +0200

    section 7 - 62. Bonus: Refactoring Other Stateless Functional Components

--- 14- The Complete React Developer Course (w Hooks and Redux)/indecision-app/src/components/Action.js
+++ 14- The Complete React Developer Course (w Hooks and Redux)/indecision-app/src/components/Action.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-const Action = (props) => {
+const Action = (props) => (
+    <div>
+        <button
+            onClick={props.handlePick}
+            disabled={!props.hasOption}
+        >
+            What should I do?
+        </button>
+    </div>
+);
 
-    return (
-        <div>
-            <button
-                onClick={props.handlePick}
-                disabled={!props.hasOption}
-            >
-                What should I do?
-            </button>
-        </div>
-    );
-
-}
 export default Action;
