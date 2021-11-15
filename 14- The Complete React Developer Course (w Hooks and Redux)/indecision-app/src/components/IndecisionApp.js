14902916290aab85604f5815e959ec8f0e717f4c
Ahmed Radi <ahmedradi743@gmail.com>
2021-11-15 19:00:08 +0200

    section 8 - 67. Theming with Variables

--- 14- The Complete React Developer Course (w Hooks and Redux)/indecision-app/src/components/IndecisionApp.js
+++ 14- The Complete React Developer Course (w Hooks and Redux)/indecision-app/src/components/IndecisionApp.js
@@ -76,18 +76,20 @@ export default class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header subTitle={header} />
-                <Action
-                    hasOption={this.state.options.length > 0}
-                    handlePick={this.handlePick}
-                />
-                <Options
-                    options={this.state.options}
-                    handleDeleteOptions = {this.handleDeleteOptions}
-                    handleDeleteOption = {this.handleDeleteOption}
-                />
-                <AddOption
-                    handleAddOption={this.handleAddOption}
-                />
+                <div className="container">
+                    <Action
+                        hasOption={this.state.options.length > 0}
+                        handlePick={this.handlePick}
+                    />
+                    <Options
+                        options={this.state.options}
+                        handleDeleteOptions = {this.handleDeleteOptions}
+                        handleDeleteOption = {this.handleDeleteOption}
+                    />
+                    <AddOption
+                        handleAddOption={this.handleAddOption}
+                    />
+                </div>
                 <OptionModal
                     selectedOption={this.state.selectedOption}
                     handleClearSelectedOption={this.handleClearSelectedOption}
