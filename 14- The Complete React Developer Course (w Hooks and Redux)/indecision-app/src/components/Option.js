a3628f1ced76a2d9d79a5548913d2a351e8c1bff
Ahmed Radi <ahmedradi743@gmail.com>
2021-11-17 21:19:39 +0200

    section 8 - 70. Styling Option Item

--- 14- The Complete React Developer Course (w Hooks and Redux)/indecision-app/src/components/Option.js
+++ 14- The Complete React Developer Course (w Hooks and Redux)/indecision-app/src/components/Option.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Option = (props) => (
-    <p>
-        {props.optionText}
+    <p className="option">
+        <p className="option__text">{props.count}. {props.optionText}</p>
+
         <button className="button button--link"
             onClick={() => {
                 props.handleDeleteOption(props.optionText)
