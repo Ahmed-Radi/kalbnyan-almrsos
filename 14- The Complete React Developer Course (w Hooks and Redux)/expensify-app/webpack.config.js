28d5ebed0e18ae540265ef8fffa6dc3ae98cef60
Ahmed Radi <ahmedradi743@gmail.com>
2021-11-22 22:51:44 +0200

    section 10 - 85. Setting up Redux & 86. Dispatching Actions

--- 14- The Complete React Developer Course (w Hooks and Redux)/expensify-app/webpack.config.js
+++ 14- The Complete React Developer Course (w Hooks and Redux)/expensify-app/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: './src/app.js', // input -> file will compile it
+    entry: './src/playground/redux.js', // input -> file will compile it
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js',
