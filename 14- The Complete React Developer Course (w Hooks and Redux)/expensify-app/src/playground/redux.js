adcffa2efb5a2e6973ded698d3465ae5c3ca41bd
Ahmed Radi <ahmedradi743@gmail.com>
2021-11-23 11:13:26 +0200

    section 10 - 87. Subscribing and Dynamic Actions

--- 14- The Complete React Developer Course (w Hooks and Redux)/expensify-app/src/playground/redux.js
+++ 14- The Complete React Developer Course (w Hooks and Redux)/expensify-app/src/playground/redux.js
@@ -2,12 +2,18 @@ import { createStore } from 'redux'
 const store = createStore((state = {count: 0 }, action) => {
     switch(action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             }
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
+            }
+        case 'SET':
+            return {
+                count: action.count
             }
         case 'REST':
             return {
@@ -18,10 +24,19 @@ const store = createStore((state = {count: 0 }, action) => {
     }
 })
 
-console.log(store.getState())
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState())
+})
 
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
+})
+
+// unsubscribe()
+
+store.dispatch({
+    type: 'INCREMENT',
 })
 
 store.dispatch({
@@ -29,7 +44,11 @@ store.dispatch({
 })
 
 store.dispatch({
-    type: 'DECREMENT'
+    type: 'DECREMENT',
+    decrementBy: 10
 })
 
-console.log(store.getState())
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
