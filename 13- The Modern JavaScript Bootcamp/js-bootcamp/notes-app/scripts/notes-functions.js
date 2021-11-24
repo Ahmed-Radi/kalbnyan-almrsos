351972b00fca0b80562f7558f0c83ea7b9e93543
Ahmed Radi <ahmedradi743@gmail.com>
2021-11-24 11:04:55 +0200

    section 12 - 124. Setting up the Notes Theme: Part II

--- 13- The Modern JavaScript Bootcamp/js-bootcamp/notes-app/scripts/notes-functions.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/notes-app/scripts/notes-functions.js
@@ -26,18 +26,9 @@ const removeNote = (id) => {
 
 // Generate the DOM structure for a note
 const generateNoteDOM = (note) => {
-    const noteEl = document.createElement('div');
-    const textEl = document.createElement('a');
-    const button = document.createElement('button');
-
-    // Setup the remove note button
-    button.textContent = 'X'
-    noteEl.appendChild(button)
-    button.addEventListener('click', () => {
-        removeNote(note.id);
-        saveNotes(notes);
-        renderNotes(notes, filters);
-    })
+    const noteEl = document.createElement('a');
+    const textEl = document.createElement('p');
+    const statusEL = document.createElement('p');
 
     // Setup the note title text
     if(note.title.length > 0) {
@@ -45,8 +36,18 @@ const generateNoteDOM = (note) => {
     } else {
         textEl.textContent = 'Unnamed Notes';
     }
-    textEl.setAttribute('href',`./edit.html#${note.id}`)
+
+    textEl.classList.add('list-item__title')
     noteEl.appendChild(textEl)
+
+    // setup the link`
+    noteEl.setAttribute('href',`./edit.html#${note.id}`)
+    noteEl.classList.add('list-item')
+
+    // setup status message
+    statusEL.textContent = generateLastEditedMessage(note.updatedAt)
+    statusEL.classList.add('list-item__subtitle')
+    noteEl.appendChild(statusEL)
 
     return noteEl;
 }
@@ -91,17 +92,24 @@ const sortNotes = (notes, sortBy) => {
 
 // Render application notes
 const renderNotes = (notes, filters) => {
-    // const notesEl = document.querySelector('#notes')
+    const notesEl = document.querySelector('#notes')
     notes = sortNotes(notes, filters.sortBy);
-    const filterNotes = notes.filter( (note) => {
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-    document.querySelector('#notes').innerHTML = '' //used to remove all notes to select specify notes only
-    //show specify notes
-    filterNotes.forEach( (note) => {
-        const noteEl = generateNoteDOM(note)
-        document.querySelector('#notes').appendChild(noteEl)
-    })
+    const filterNotes = notes.filter( (note) => note.title.toLowerCase().includes(filters.searchText.toLowerCase()))
+
+    notesEl.innerHTML = '' //used to remove all notes to select specify notes only
+
+    if(filterNotes.length > 0) {
+        //show specify notes
+        filterNotes.forEach( (note) => {
+            const noteEl = generateNoteDOM(note)
+            notesEl.appendChild(noteEl)
+        })
+    } else {
+        const emptyMessage = document.createElement('p');
+        emptyMessage.textContent = 'No notes to show.'
+        emptyMessage.classList.add('empty-message')
+        notesEl.appendChild(emptyMessage)
+    }
 }
 
 // Generate the last edited message
