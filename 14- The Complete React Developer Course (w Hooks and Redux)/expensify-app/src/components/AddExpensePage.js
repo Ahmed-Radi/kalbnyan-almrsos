40dee6eb74d0c5805994f2d9ccc969567b852c94
Ahmed Radi <ahmedradi743@gmail.com>
2021-12-05 23:17:23 +0200

    section 11 - 107. Wiring up Add Expense

--- 14- The Complete React Developer Course (w Hooks and Redux)/expensify-app/src/components/AddExpensePage.js
+++ 14- The Complete React Developer Course (w Hooks and Redux)/expensify-app/src/components/AddExpensePage.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { useNavigate  } from "react-router-dom";
+import { addExpense } from '../actions/expenses';
 import ExpenseForm from './ExpenseForm';
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => {
+
+    const navigate = useNavigate();
+
+    return (
     <div>
         <h1>Add Expense</h1>
-        <ExpenseForm />
+        <ExpenseForm
+            onSubmit={(expense)=> {
+                if (expense.description && expense.amount) {
+                    props.dispatch(addExpense(expense));
+                    navigate('/');
+                }
+            }}
+        />
     </div>
-)
+)}
 
-export default AddExpensePage
+export default connect()(AddExpensePage);
--- 14- The Complete React Developer Course (w Hooks and Redux)/expensify-app/src/components/ExpenseForm.js
+++ 14- The Complete React Developer Course (w Hooks and Redux)/expensify-app/src/components/ExpenseForm.js
@@ -4,8 +4,6 @@ import { SingleDatePicker } from 'react-dates'
 import 'react-dates/lib/css/_datepicker.css'
 import 'react-dates/initialize';
 
-const now = moment()
-console.log(now.format('MMM Do, YYYY'))
 export default class ExpenseForm  extends Component {
     state = {
         description: '',
@@ -13,6 +11,7 @@ export default class ExpenseForm  extends Component {
         amount: '',
         createdAt: moment(),
         calenderFocused: false,
+        error: '',
     }
     onDescriptionChange = ( e ) => {
         const description = e.target.value;
@@ -24,20 +23,38 @@ export default class ExpenseForm  extends Component {
     }
     onAmountChange = ( e ) => {
         const amount = e.target.value;
-        if (amount.match(/^\d*(\.\d{0,2})?$/gm)) {
+        if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/gm)) {
             this.setState(() => ({ amount }))
         }
     };
     onDateChange = (createdAt) => {
-        this.setState(() => ({ createdAt }))
+        if (createdAt) {
+            this.setState(() => ({ createdAt }))
+        }
     }
     onFocusChange = ({ focused }) => {
         this.setState(() => ({ calenderFocused: focused }))
     }
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (!this.state.description || !this.state.amount) {
+            this.setState(()=> ({error: 'Please Enter Description and amount '}))
+        } else {
+            this.state.error
+            this.setState(()=> ({error: ''}))
+        }
+        this.props.onSubmit({
+            description: this.state.description,
+            amount: parseFloat(this.state.amount, 10) * 100,
+            createdAt: this.state.createdAt.valueOf(),
+            note: this.state.note
+        })
+    }
     render() {
         return (
             <div>
-                <form>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.onSubmit}>
                     <input
                         type="text"
                         placeholder="Description"
