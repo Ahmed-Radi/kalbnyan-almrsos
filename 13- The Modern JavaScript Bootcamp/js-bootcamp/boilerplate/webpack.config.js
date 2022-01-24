9d5047fb762384a9aad2aba2dbde00f793116db7
Ahmed Radi <ahmedradi743@gmail.com>
2022-01-24 20:20:47 +0200

    section 13 - 137. Adding Babel into Webpack

--- 13- The Modern JavaScript Bootcamp/js-bootcamp/boilerplate/webpack.config.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/boilerplate/webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
         path: path.resolve(__dirname, 'public/scripts'),
         filename: 'bundle.js'
     },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [{
+                loader:'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }]
+        }]
+    },
     // mode: 'development',
 }
