2af2b9b9661854f3d8d0e58267dec165bb3d9c09
Ahmed Radi <ahmedradi743@gmail.com>
2022-02-20 16:18:28 +0200

    section 13 - 144. Converting Notes App: Part III

--- 13- The Modern JavaScript Bootcamp/js-bootcamp/notes-app/src/index.js
+++ 13- The Modern JavaScript Bootcamp/js-bootcamp/notes-app/src/index.js
@@ -1,5 +1,15 @@
-import { getNotes, createNote } from './notes'
+import { getNotes, createNote, removeNote, updateNote } from './notes'
+import { getFilters, setFilters } from './filters'
 
-console.log(getNotes())
-createNote()
-console.log(getNotes())
+// console.log(getNotes())
+// createNote()
+// removeNote('f4ad31a9-0a52-413d-9192-44ceb2aac5e5')
+// updateNote('3508f896-fff2-4a5f-85cc-d9cff41e2f21', {title: 'Ahmed Radi', body: 'go do it'})
+// console.log(getNotes())
+
+console.log(getFilters())
+setFilters({
+    searchText: 'Ahmed',
+    sortBy: 'byCreated'
+})
+console.log(getFilters())
