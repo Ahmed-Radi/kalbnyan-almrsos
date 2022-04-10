45bfb03259d3e0b1fb1831f1020989f42d20e417
Ahmed Radi <ahmedradi743@gmail.com>
2022-04-10 14:09:56 +0200

    section 3 - 16. Argument Parsing with Yargs: Part I

--- 15-The Complete Node.js Developer Course(3rd Edition)/notes-app/app.js
+++ 15-The Complete Node.js Developer Course(3rd Edition)/notes-app/app.js
@@ -1,5 +1,41 @@
 const notes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs')
 
-console.log(chalk.black.bgGreen.inverse.bold('Hello world!'));
-console.log(notes())
+//add
+yargs.command({
+    command: 'add',
+    describe: 'Add New Note',
+    handler: function () {
+        console.log('Add note')
+    }
+})
+
+//remove
+yargs.command({
+    command: 'remove',
+    describe: 'remove note',
+    handler: function () {
+        console.log('remove note')
+    }
+})
+
+//list
+yargs.command({
+    command: 'list',
+    describe: 'list note',
+    handler: function () {
+        console.log('list note')
+    }
+})
+
+//read
+yargs.command({
+    command: 'read',
+    describe: 'read note',
+    handler: function () {
+        console.log('read note')
+    }
+})
+
+console.log(yargs.argv)
